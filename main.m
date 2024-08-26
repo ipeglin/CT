aab6c4bf324ca4d3d337ae8c4d3b31920a1a7606
ipeglin <philip@eglin.no>
2024-08-26 16:56:45 +1000

    feat: adding code up to C3

--- main.m
+++ main.m
@@ -1,37 +1,35 @@
-%% Original image
-load('original_image_128.mat')
-figure;
-imagesc(original_image)
-axis image; % Ensure the aspect ratio is correct
-colormap gray; % Use a grayscale colormap
-colorbar; % Show a colorbar
-title('Original Image');
-xlabel('Position [x]');
-ylabel('Position [y]');
-
+clear all
 %% Load Sinogram
-load('128_18.mat')
-figure;
-imagesc(sino)
-colormap gray; % Use a grayscale colormap
-colorbar; % Show a colorbar
-title('CT Scan Sinogram');
-xlabel('Angle [\theta]');
-ylabel('Position [x]');
+
+
+% load('128_18.mat') % Part A
+load('128_60.mat') % C3
+
+
+% figure;
+% imagesc(linspace(-63, 64, gridSize), linspace(-63, 64, gridSize), sino)
+% colormap gray; % Use a grayscale colormap
+% colorbar; % Show a colorbar
+% title('CT Scan Sinogram');
+% xlabel('Angle [\theta]');
+% ylabel('Position [x]');
 
 %% General variables
 [numProjections, numAngles] = size(sino);
 
-angleInterval = [30, 180];
-angles = linspace(angleInterval(1), angleInterval(2), numAngles-2);
+angleInterval = [10, 180];
+angles = linspace(angleInterval(1), angleInterval(2), numAngles);
+angles
 
 gridSize = 128;
 l = -92:92;
 
 %% A2
-angleSample = 1;
-projectionAngle = angles(angleSample);
+angleSample = 3;
+projectionAngle = 30;
 projectionAngleRad = deg2rad(projectionAngle);
+disp(['Using angle ' num2str(projectionAngle) ' in rad ' projectionAngleRad]);
+disp(['Using angle index ' num2str(angleSample) ' corresponding to ' num2str(angles(angleSample))])
 
 p = sino(:, angleSample);
 
@@ -63,18 +61,18 @@ p = sino(:, angleSample);
 l_cartesian = x * cos(projectionAngleRad) + y * sin(projectionAngleRad);
 b30 = interp1(l, p, l_cartesian, 'linear', 0);
 
-figure;
-imagesc(linspace(-63, 64, gridSize), linspace(-63, 64, gridSize), b30);
-axis image; % Ensure the aspect ratio is correct
-colormap gray; % Use a grayscale colormap
-colorbar; % Show a colorbar
-title('2D Back Projection b(x,y)');
-subtitle('Using meshgrid');
-xlabel('Position [x]');
-ylabel('Position [y]');
+% figure;
+% imagesc(linspace(-63, 64, gridSize), linspace(-63, 64, gridSize), b30);
+% axis image; % Ensure the aspect ratio is correct
+% colormap gray; % Use a grayscale colormap
+% colorbar; % Show a colorbar
+% title('2D Back Projection b(x,y)');
+% subtitle('Using meshgrid');
+% xlabel('Position [x]');
+% ylabel('Position [y]');
 
 %% A3
-image = zeros(gridSize, gridSize);
+image_a3 = zeros(gridSize, gridSize);
 for idx = 1:numel(angles)
   projectionAngle = angles(idx);
   projectionAngleRad = deg2rad(projectionAngle);
@@ -84,23 +82,23 @@ for idx = 1:numel(angles)
   [x,y] = meshgrid(linspace(-63, 63, gridSize));
   l_cartesian = x * cos(projectionAngleRad) + y * sin(projectionAngleRad);
   backProjection = interp1(l, forward_projection, l_cartesian, 'linear', 0);
-  image = image + backProjection;
+  image_a3 = image_a3 + backProjection;
 end
 
-image = (pi / (2 * numAngles)) * image;
+image_a3 = (pi / (2 * numAngles)) * image_a3;
 
-figure;
-imagesc(linspace(-63, 64, gridSize), linspace(-63, 64, gridSize), image);
-axis image; % Ensure the aspect ratio is correct
-colormap gray; % Use a grayscale colormap
-colorbar; % Show a colorbar
-title('Back Projection image f_b(x,y)');
-xlabel('Position [x]');s
-ylabel('Position [y]');
+% figure;
+% imagesc(linspace(-63, 64, gridSize), linspace(-63, 64, gridSize), image_a3);
+% axis image; % Ensure the aspect ratio is correct
+% colormap gray; % Use a grayscale colormap
+% colorbar; % Show a colorbar
+% title('Back Projection image f_b(x,y)');
+% xlabel('Position [x]');
+% ylabel('Position [y]');
 
 %% B2
 
-image = zeros(gridSize, gridSize);
+image_b2 = zeros(gridSize, gridSize);
 
 omega = linspace(-1, 1, numProjections);
 rampFilter = abs(omega);
@@ -120,21 +118,59 @@ for idx = 1:numel(angles)
 
     backprojected = interp1(l, p_filtered, s, 'linear', 0);
     
-    image = image + backprojected;
+    image_b2 = image_b2 + backprojected;
 end
 
 % Normalize the reconstructed image
-image = (pi / (2 * numAngles)) * image;
+image_b2 = (pi / (2 * numAngles)) * image_b2;
 
-% Display the reconstructed image
-figure;
-imagesc(linspace(-63, 64, gridSize), linspace(-63, 64, gridSize), image);
-axis image; % Ensure the aspect ratio is correct
-colormap gray; % Use a grayscale colormap
-colorbar; % Show a colorbar
-title('Filtered Back Projection Image');
-subtitle('Using ramp filter \omega \in [-1, 1]')
-xlabel('Position [x]');
-ylabel('Position [y]');
+% figure;
+% imagesc(linspace(-63, 64, gridSize), linspace(-63, 64, gridSize), image_b2);
+% axis image; % Ensure the aspect ratio is correct
+% colormap gray; % Use a grayscale colormap
+% colorbar; % Show a colorbar
+% title('Filtered Back Projection Image');
+% subtitle('Using ramp filter \omega \in [-1, 1]')
+% xlabel('Position [x]');
+% ylabel('Position [y]');
 
-%% B3
+%% C1
+% Original image
+load('original_image_128.mat')
+% figure;
+% imagesc(linspace(-63, 64, gridSize), linspace(-63, 64, gridSize), original_image)
+% axis image; % Ensure the aspect ratio is correct
+% colormap gray; % Use a grayscale colormap
+% colorbar; % Show a colorbar
+% title('Original Image');
+% xlabel('Position [x]');
+% ylabel('Position [y]');
+
+mse_a3 = immse(image_a3, original_image);
+mse_b2 = immse(image_b2, original_image);
+
+disp(['MSE for A3: ' num2str(mse_a3)]);
+disp(['MSE for B2: ' num2str(mse_b2)]);
+
+%% C2
+controlImage = iradon(sino, angles);
+imageSize = size(controlImage);
+
+% slice away outer rows
+controlImage = controlImage(2:imageSize(1) - 1,:);
+% slice away outer cols
+controlImage = controlImage(:, 2:imageSize(2) - 1);
+
+mse_iradon = immse(controlImage, original_image);
+disp(['MSE for inverse Radon: ' num2str(mse_iradon)]);
+
+% figure;
+% imagesc(linspace(-63, 64, gridSize), linspace(-63, 64, gridSize), controlImage)
+% axis image; % Ensure the aspect ratio is correct
+% colormap gray; % Use a grayscale colormap
+% colorbar; % Show a colorbar
+% title('Original Image');
+% subtitle('Reconstruction using iradon');
+% xlabel('Position [x]');
+% ylabel('Position [y]');
+% 
